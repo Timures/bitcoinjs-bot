800f2fe84479f58f1815970323971acf628df0a0
James328 <jhume328@yahoo.com>
2014-08-30 04:29:46 -0500

    Incorporated coinbaseSell / Spot Rate data and built a basic Bootstrap front-end

--- Readme.md
+++ Readme.md
@@ -4,3 +4,13 @@ to start Mongo database on local
 
 mongo
 to start queirying the database
+
+
+Querying the db:
+
+db.tweets.find()[0]
+db.tweets.find({sentiment: {$lt: 0}}).length()
+
+
+ps -eaf|grep node
+kill -9 process_id
--- app.js
+++ app.js
@@ -1,6 +1,47 @@
 var config       = require('./config');
 var TweetScraper = require('./lib/tweet_scraper');
-var CoinbaseScraper = require('./lib/coinbase_scraper');
+var CoinbaseScraperBuy = require('./lib/coinbase_scraper_buy');
+var CoinbaseScraperSell = require('./lib/coinbase_scraper_sell');
+var CoinbaseScraperSpotRate = require('./lib/coinbase_scraper_spotrate');
+
+var express = require('express');
+var app = express();
+var db = require("mongojs").connect(config.dbInfo.url, config.dbInfo.collections);
+
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+app.set('port', (process.env.PORT || 8000));
 
 TweetScraper.start();
-CoinbaseScraper.start();
+CoinbaseScraperBuy.start();
+CoinbaseScraperSell.start();
+CoinbaseScraperSpotRate.start();
+
+
+app.get('/', function(req, res) {
+	db.tweets.find(function(err, tweets) {
+		var allTweetsLength = tweets.length;
+
+		var positive = tweets.filter(function(tweet) {
+			return tweet.sentiment > 0;
+		}).length
+
+		var negative = tweets.filter(function(tweet) {
+			return tweet.sentiment < 0;
+		}).length
+
+		var neutral = tweets.filter(function(tweet) {
+			return tweet.sentiment === 0;
+		}).length
+		res.render('index.html', {
+			tweetsGotten: allTweetsLength,
+			negative: negative,
+			positive: positive,
+			neutral: neutral,
+		});
+	});
+});
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:", app.get('port'));
+});
