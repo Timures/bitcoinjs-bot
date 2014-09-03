6c9858475d7d8e88104e535c21728c3ff5db787a
James328 <jhume328@yahoo.com>
2014-09-03 10:34:53 -0500

    Built out front-end for Coinbase API

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
+// Dependencies
 var config          = require('./config');
 var TweetScraper    = require('./lib/tweet_scraper');
 var CoinbaseScraper = require('./lib/coinbase_scraper');
+var db              = require("mongojs").connect(config.dbInfo.url, config.dbInfo.collections);
+var express         = require('express');
+var app             = express();
 
-var db = require("mongojs").connect(config.dbInfo.url, config.dbInfo.collections);
-var express = require('express');
-var app = express();
-
-app.set('front', __dirname + '/front');
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
 app.engine('css', require('ejs').renderFile);
 app.set('port', (process.env.PORT || 8000));
 
+// Startup modules for Coinbase and Twitter
 TweetScraper.start();
 CoinbaseScraper.start({
   buyPrice:   true,
@@ -19,20 +21,55 @@ CoinbaseScraper.start({
   historical: true
 })
 
+app.get('/', function(req, res) {
+  res.render('./index');
+});
+
+// Renders Twitter http://localhost:8000/tweets JSON API 
 app.get('/tweets', function(req, res) {
-  db.tweets.find( {}, { limit : 1000 }, function(err, tweets) {
+  db.tweets.find( {}, { limit: 200, sort: { 'timestamp': -1 } }, function(err, tweets) {
     res.json(tweets);
   });
 });
 
-app.get('/', function(req, res) {
-  res.render('../front/twitter.html');
+// Main Twitter page showing latest tweets
+app.get('/twitter', function(req, res) {
+  res.render('./twitter.ejs');
+});
+
+//  Renders Coinbase http://localhost:8000/coinbaseHistorical JSON API
+app.get('/coinbaseHistorical', function(req, res) {
+  db.coinbaseHistorical.find( {}, { limit: 200, sort: { 'date': -1 } }, function(err, coinbaseHistorical) {
+    res.json(coinbaseHistorical);
+  });
+});
+
+app.get('/coinbaseBuy', function(req, res) {
+  db.coinbaseBuy.find( {}, { limit: 200, sort: { '_id': -1 } }, function(err, coinbaseBuy) {
+    res.json(coinbaseBuy);
+  });
+});
+
+app.get('/coinbaseSell', function(req, res) {
+  db.coinbaseSell.find( {}, { limit: 200, sort: { '_id': -1 } }, function(err, coinbaseSell) {
+    res.json(coinbaseSell);
+  });
+});
+
+app.get('/coinbaseSpotRate', function(req, res) {
+  db.coinbaseSpotRate.find( {}, { limit: 200, sort: { '_id': -1 } }, function(err, coinbaseSpotRate) {
+    res.json(coinbaseSpotRate);
+  });
+});
+
+// Main Coinbase page showing latest historical prices
+app.get('/coinbase', function(req, res) {
+  res.render('./coinbase.ejs');
 });
 
 app.get('/stylesheet.css', function(req, res) {
-  res.render('../front/stylesheet.css');
+  res.render('../views/stylesheet.css');
 });
-
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:", app.get('port'));
